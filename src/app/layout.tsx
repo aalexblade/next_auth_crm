59bc855a4410edcc38aa2a422af763eef2b3cb0f
alex blade <aalexblade@gmail.com>
2026-03-27 21:18:45 +0200

    add sidebar to loyouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
+
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning>{children}</body>
+      <body className={font.className} suppressHydrationWarning>{children}</body>
     </html>
   );
 }
